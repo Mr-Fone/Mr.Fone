4b0f556641aef1332ba5c61f8355bdbdfe325f9b
shaamilshan <shmlvk123@gmail.com>
2022-08-17 19:26:45 +0100

    pshh

---
+++ main.js
@@ -0,0 +1,20 @@
+const a_tag = document.querySelector("#sms")
+
+function getMobileOperatingSystem() {
+  var userAgent = navigator.userAgent || navigator.vendor || window.opera;
+
+
+  if (/android/i.test(userAgent)) {
+    a_tag.href = "sms://num?body="
+
+  }
+
+  // iOS detection from: http://stackoverflow.com/a/9039885/177710
+  if (/iPad|iPhone|iPod/.test(userAgent) && !window.MSStream) {
+    a_tag.href = "sms://;?&body="
+  }
+
+}
+
+getMobileOperatingSystem();
+console.log(a_tag.getAttribute("href"));
