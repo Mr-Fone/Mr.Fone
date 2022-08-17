881bc42457dd36d996567ac7c0f318aa4197ac41
shaamilshan <shmlvk123@gmail.com>
2022-08-17 17:12:33 +0100

    js

---
+++ java.js
@@ -0,0 +1,13 @@
+
+function getMobileOperatingSystem(){
+    var userAgent = navigator.userAgent|| navigator.vendor || window.getMobileOperatingSystem;
+    if (/windows phone/i.test(userAgent)){
+        return window.location.href = index.html
+    }
+    if(/android/i.test(userAgent)){
+        return window.location.href =index.html
+    }
+    if(/iPad|iPhone|iPod/.test(userAgent)&& !window.MSStream){
+        return window.location.href =index.html
+    }
+}
