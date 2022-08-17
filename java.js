1627da8c80c568405494c8e041dff5bc11010931
shaamilshan <shmlvk123@gmail.com>
2022-08-17 17:19:04 +0100

    vivi

--- java.js
+++ java.js
@@ -2,10 +2,10 @@
 function getMobileOperatingSystem(){
     var userAgent = navigator.userAgent|| navigator.vendor || window.getMobileOperatingSystem;
     if (/windows phone/i.test(userAgent)){
-        return window.location.href = index.html
+        return window.location.href = android.html
     }
     if(/android/i.test(userAgent)){
-        return window.location.href =index.html
+        return window.location.href =("android.html")
     }
     if(/iPad|iPhone|iPod/.test(userAgent)&& !window.MSStream){
         return window.location.href =index.html
